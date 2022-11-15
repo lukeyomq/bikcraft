75079f80b1abbb89f7a439152994cd0dbed5bb85
lukeyomq <luke_1999queiroz@hotmail.com>
2022-11-15 13:22:03 -0300

    implementação js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Atiuvar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url  = location.href;
+    const href = link.href;
+    if (url.includes(href)) {
+        link.classList.add("ativo");
+    }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const element = document.getElementById(parametro);
+    if (parametro) {
+        element.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
